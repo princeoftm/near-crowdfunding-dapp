e533670abd4484e13457375a2d951c153054cf99
Aniruddh Rao <raoaniruddh92@gmail.com>
2025-06-18 16:57:27 +0530

    push

--- contract/target/release/deps/borsh_derive-dbd0871ca5ffa1e8.d
+++ contract/target/release/deps/borsh_derive-dbd0871ca5ffa1e8.d
@@ -1,5 +1,5 @@
-/Users/aniruddhrao/Documents/GitHub/near-crowdfunding-dapp/contract/target/release/deps/libborsh_derive-dbd0871ca5ffa1e8.dylib: /Users/aniruddhrao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/borsh-derive-0.8.2/src/lib.rs
+/Users/aniruddhrao/openfund/contract/target/release/deps/libborsh_derive-dbd0871ca5ffa1e8.dylib: /Users/aniruddhrao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/borsh-derive-0.8.2/src/lib.rs
 
-/Users/aniruddhrao/Documents/GitHub/near-crowdfunding-dapp/contract/target/release/deps/borsh_derive-dbd0871ca5ffa1e8.d: /Users/aniruddhrao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/borsh-derive-0.8.2/src/lib.rs
+/Users/aniruddhrao/openfund/contract/target/release/deps/borsh_derive-dbd0871ca5ffa1e8.d: /Users/aniruddhrao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/borsh-derive-0.8.2/src/lib.rs
 
 /Users/aniruddhrao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/borsh-derive-0.8.2/src/lib.rs:
--- contract/target/release/deps/proc_macro_crate-7cb8f1f68a4901a6.d
+++ contract/target/release/deps/proc_macro_crate-7cb8f1f68a4901a6.d
@@ -1,7 +1,7 @@
-/Users/aniruddhrao/Documents/GitHub/near-crowdfunding-dapp/contract/target/release/deps/libproc_macro_crate-7cb8f1f68a4901a6.rmeta: /Users/aniruddhrao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro-crate-0.1.5/src/lib.rs
+/Users/aniruddhrao/openfund/contract/target/release/deps/libproc_macro_crate-7cb8f1f68a4901a6.rmeta: /Users/aniruddhrao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro-crate-0.1.5/src/lib.rs
 
-/Users/aniruddhrao/Documents/GitHub/near-crowdfunding-dapp/contract/target/release/deps/libproc_macro_crate-7cb8f1f68a4901a6.rlib: /Users/aniruddhrao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro-crate-0.1.5/src/lib.rs
+/Users/aniruddhrao/openfund/contract/target/release/deps/libproc_macro_crate-7cb8f1f68a4901a6.rlib: /Users/aniruddhrao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro-crate-0.1.5/src/lib.rs
 
-/Users/aniruddhrao/Documents/GitHub/near-crowdfunding-dapp/contract/target/release/deps/proc_macro_crate-7cb8f1f68a4901a6.d: /Users/aniruddhrao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro-crate-0.1.5/src/lib.rs
+/Users/aniruddhrao/openfund/contract/target/release/deps/proc_macro_crate-7cb8f1f68a4901a6.d: /Users/aniruddhrao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro-crate-0.1.5/src/lib.rs
 
 /Users/aniruddhrao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro-crate-0.1.5/src/lib.rs:
